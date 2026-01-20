2c10d176532543147365e6dea16ce1d467e823dd
Tamirucode <tam.egu76@gmail.com>
2026-01-20 18:13:08 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//const form = document.getElementById('survey-form');
+  
+ // form.addEventListener('submit', (e) => {
+   // e.preventDefault(); // Prevents the page from refreshing
+   // const formData = new FormData(form);
+    //const result = formData.get('satisfaction');
+    
+    //alert('You selected: ' + result);
+    //Get the radio value
+   // const satisfaction = formData.get('satisfaction');
+    //Get all checked boxes value for 'features'
+   // const features = formData.getAll('features');
+
+   // console.log('Satisfaction:',satisfaction);
+   // console.log('Selected Features:',features);
+
+   // alert(`Thankyou!\nSatisfaction: ${satisfaction}\nFeatures:${features.join(',')}`)
+ 
+   // });
